f2454083cfbe1304d34983d9c57391f0e59e6151
Shagufa Ali <shagufa.ali@boldcommerce.com>
2022-09-02 18:35:45 +0000

    CE-502-Payment line item display fix

--- src/stripe/changeStripeShippingLines.ts
+++ src/stripe/changeStripeShippingLines.ts
@@ -1,22 +1,23 @@
 import {changeShippingLine, getApplicationState} from '@bold-commerce/checkout-frontend-library';
-import {API_RETRY, IStripeEvent, IStripeShippingOptions} from 'src/types';
+import {API_RETRY, IStripeEvent, IStripeShippingOptions, getStripeDisplayItem} from 'src';
 
 
 export async function changeStripeShippingLines(event: IStripeEvent): Promise<void> {
     const shippingOption = event.shippingOption as IStripeShippingOptions;
     const response = await changeShippingLine(shippingOption.id, API_RETRY);
+    const displayItems = getStripeDisplayItem();
 
     if(response.success){
         const {order_total} = getApplicationState();
         const total = {
             label: 'Total',
-            amount: order_total,
-            pending: true,
+            amount: order_total
         };
 
         event.updateWith({
             total: total,
             status: 'success',
+            displayItems: displayItems
         });
     } else {
         event.updateWith({
